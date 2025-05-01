ffb339a130161127443d6ee5a6dd92160b373548
raphaelDupuy <dupuy.raphael@yahoo.com>
2025-05-01 21:55:23 +0200

    Modifs structures

--- main.c
+++ main.c
@@ -6,66 +6,70 @@
 typedef int indice;
 typedef float proba;
 
-int M; // Nb elements non nuls
-int C; // Nb colonnes
 proba alpha = 0.85; // Proba du surfeur aléatoire
 proba invAlpha = 1 - 0.85;
 
 struct elem {
     indice i, j; // Les indexes
-    proba val; // La valeur
+    proba val;   // La valeur
 };
 
-struct elem *P; // Matrice stochastique
+struct matrice {    // Matrice carrée
+    struct elem *P; // Ensemble des (i, j, val)
+    indice C;       // Nombre de colonnes (et lignes, matrice carrée)
+    int M;          // Nombre d'elements non nuls (de *P)
+};
+
+struct vecteur {
+    proba *v;
+    indice C;
+};
+
 struct timeval t1, t2;
 
 int *f; // Vecteur ligne de taille N tel que f[i] = 1 si la ligne i de P ne contient que des zéros et sinon f[i] = 0
-proba *w;
-proba *x;
-proba *y;
-proba *z;
 
-void initvec(proba *z, float val) {
-    indice i; for (i = 0; i<C; i++) { z[i] = val;}
+struct vecteur *alloueVecteur(int taille) {
+    struct vecteur *Z = malloc(sizeof(struct vecteur)); if(!Z) { exit(420); }
+    Z->C = taille;; if (!Z->C) { exit(422); }
+    Z->v = malloc(taille * sizeof(proba)); if (!Z->v) { exit(421); }
+    return Z;
 }
 
-void initvecStoc(proba *z, float val) {
-    initvec(z, (double) C);
+void initvec(struct vecteur *Vec, float val, int taille) {
+    indice i; for (i = 0; i < taille; i++) { Vec->v[i] = val; }
 }
 
-void calculeF(int *f, struct elem *P) {
-    if (f == NULL) { exit(50); }
-    for (indice i = 0; i<C; i++) {
+void calculeF(int *f, struct matrice *Mat) {
+    for (indice i = 0; i<Mat->C; i++) {
         f[i] = 1;
     }
     int ligne;
-    for (indice k = 0; k < M; k++) {
-        ligne = P[k].i;
+    for (indice k = 0; k < Mat->M; k++) {
+        ligne = Mat->P[k].i;
         if (f[ligne]){
             f[ligne] = 0;
         }
     }
 }
 
-void affichevec(proba *z) {
-    indice i; for (i = 0; i<C; i++) {printf("%f ", z[i]);}
+void affichevec(struct vecteur *Vec) {
+    printf("Affichage vecteur\n");
+    if (!Vec || !Vec->v) { exit(6); }
+    indice i; for (i = 0; i < Vec->C; i++) {printf("%f ", Vec->v[i]);}
     printf("\n");
 }
 
-void affichevecInt(int *f) {
-    indice i; for (i = 0; i<C; i++) {printf("%d ", f[i]);}
-    printf("\n");
-}
-
-void affichemat(struct elem *P) {
-    indice k; for (k = 0; k < M; k++) {{printf("%d %d %f\n", P[k].i, P[k].j, P[k].val);}
+void affichemat(struct matrice Mat) {
+    indice k; for (k = 0; k < Mat.M; k++) {{printf("%d %d %f\n", Mat.P[k].i, Mat.P[k].j, Mat.P[k].val);}
     }
 }
 
-struct elem* lectureMatrice(char *nom_fic) {
+struct matrice *lectureMatrice(char *nom_fic) {
     printf("Lecture Matrice\n");
     indice i, j;
-    int nb;
+    int nb, C, M;
+    struct matrice *Mat = malloc(sizeof(struct matrice));
     struct elem *P;
     double v;
     FILE *F;
@@ -83,14 +87,11 @@ struct elem* lectureMatrice(char *nom_fic) {
     for (int k = 0; k < C; k++) {
         fscanf(F, "%d", &(i));
         fscanf(F,"%d",  &(nb)); 
-        //printf("%d\n", i);
-        //printf("nombre attendu : %d\n", nb);
         for (int z = 0; z < nb; z++) {
             struct elem e;
 
             fscanf(F, "%d", &(j));
             fscanf(F, "%le", &(v));
-            //printf("elem: %d, %f\n", j, v);
             e.i = i - 1;
             e.j = j - 1;
             e.val = v;
@@ -99,27 +100,34 @@ struct elem* lectureMatrice(char *nom_fic) {
             if (courant > M) {
                 exit(202);
             }
-            //printf("\n");
         }
     }
     printf("Nombre d'elements attendu: %d, nombre réel: %d\n", M, courant);
     fclose(F);
-    return P;
+
+    Mat->C = C;
+    Mat->M = M;
+    Mat->P = P;
+    return Mat;
 }
 
-proba* diffVect(proba *x, proba *y) {
+proba *diffVect(struct vecteur *X, struct vecteur *Y) {
     indice k;
-    proba *z = malloc(C * sizeof(float));
-
+    int C = X->C;
+    if (C != Y->C) { exit(320); }
+    proba *Z = malloc(C * sizeof(proba));
     for (k = 0; k < C; k++) {
-        z[k] = x[k] - y[k];
+        Z[k] = X->v[k] - Y->v[k];
     }
-    return z;
+    return Z;
 }
 
-float normeVect(proba *x, proba *y) {
+float normeVect(struct vecteur *X, struct vecteur *Y) {
     indice k;
-    proba* delta = diffVect(x, y);
+    int C = X->C;
+    if (C != Y->C) { exit(320); }
+
+    proba* delta = diffVect(X, Y);
     float norme = 0;
     for (k = 0; k < C; k++) {
         norme += fabs(delta[k]);
@@ -129,124 +137,135 @@ float normeVect(proba *x, proba *y) {
 }
 
 // Addition entre deux vecteurs de proba (ajoute dans y)
-void addVect(proba *x, proba *y) {
-    indice i; for (i = 0; i < C; i++) {y[i] += x[i];}
+void addVect(struct vecteur *X, struct vecteur *Y) {
+    int C = X->C;
+    if (C != Y->C) { exit(320); }
+    indice i; for (i = 0; i < C; i++) {Y->v[i] += X->v[i];}
 }
 
 // Multiplie un vecteur de proba par un float
-proba* multFloatProba(float a, proba *x) {
-    indice i;
-    proba *res = malloc(C * sizeof(proba)); if (res == NULL) {exit(40);}
-    for (i = 0; i < C; i++) {
-        res[i] = a * x[i];
+struct vecteur *multFloatProba(float a, struct vecteur *Vec) {
+    int taille = (int) Vec->C;
+    struct vecteur *res = alloueVecteur(taille);
+    for (indice i = 0; i < taille; i++) {
+        res->v[i] = a * Vec->v[i];
     }
     return res;
 }
 
 // Multiplie un vecteur de proba (horizontal) avec un vecteur de int (vertical)
-float multVectPVectI(proba *x, int *f) {
+float multVectPVectI(struct vecteur *Vec, int *f) {
     indice i;
     float res = 0.;
-    for (i = 0; i<C; i++) {
-        res += x[i] * f[i];
+    for (i = 0; i < Vec->C; i++) {
+        res += Vec->v[i] * f[i];
     }
     return res;
 }
 
 // Multiplie à gauche une matrice par un vecteur
-void multVecMat(proba *x, struct elem *P, proba *y) {
+void multVecMat(struct vecteur *X, struct matrice *Mat, struct vecteur *Y) {
     indice i, j, k;
-    for (k = 0; k < M; k++) {
-        i = P[k].i;
-        j = P[k].j;
-        float val = P[k].val;
-        y[j] += x[i] * val;
+    if ((X->C != Y->C) ||(Y->C != Mat->C)) { exit(321); }
+    for (k = 0; k < Mat->M; k++) {
+        i = Mat->P[k].i;
+        j = Mat->P[k].j;
+        float val = Mat->P[k].val;
+        Y->v[j] += X->v[i] * val;
     }
 }
 
-void metZero(proba *y) {
+void metZero(struct vecteur *Vec) {
     indice i;
-    for (i = 0; i < C; i++) {
-        y[i] = 0.0;
+    for (i = 0; i < Vec->C; i++) {
+        Vec->v[i] = 0.0;
     }
 }
 
-void recopie(proba *x, proba *y) {
+void recopie(struct vecteur *X, struct vecteur *Y) {
     indice i;
+    int C = X->C;
+    if (C != Y->C) { exit(320); }
     for (i = 0; i < C; i++) {
-        x[i] = y[i];
+        X->v[i] = Y->v[i];
     }
 }
 
-void iterer(proba *x, proba *y, proba *w, struct elem *P) {
-    proba *aX = multFloatProba(alpha, x);
+// struct elem *genereErdos(indice lignes, proba p) {
+//     struct elem *P = malloc(1);
+//     for () {
+// 
+//     }
+// }
 
-    multVecMat(aX, P, y);
+void iterer(struct vecteur *X, struct vecteur *Y, struct vecteur *W, struct matrice *Mat) {
+    struct vecteur *aX = multFloatProba(alpha, X);
 
-    proba gauche = invAlpha / (float) C;
-    proba droite = (alpha / (float) C) * multVectPVectI(x, f);
+    multVecMat(aX, Mat, Y);
 
-    initvec(w, gauche + droite);
-    addVect(w, y);
+    proba gauche = invAlpha / (float) Mat->C;
+    proba droite = (alpha / (float) Mat->C) * multVectPVectI(X, f);
+
+    initvec(W, gauche + droite, Mat->C);
+    addVect(W, Y);
 }
 
-proba* puissances(struct elem *P, proba *x) {
-    if (!x) {
+struct vecteur *puissances(struct matrice *Mat, struct vecteur *Pi) {
+    int C = Mat->C;
+    printf("%d\n", C);
+    if (!Pi) {
 
-        x = malloc(C * sizeof(proba));
-        initvec(x, 1/(double) C);  // x0 = (1/N)e
+        Pi = alloueVecteur(C);
+        initvec(Pi, 1/(double) C, C);  // x0 = (1/N)e
 
     } else {  // adapter en fonction du nombre de nouveaux elements
-        
-        z = malloc(C * sizeof(proba));
+        if (C != Pi->C) { exit(320); }
+        struct vecteur *Z = alloueVecteur(C);
 
         for (indice i = 0; i < C; i++) {
-            if (x[i]) {
-                z[i] = x[i];
+            if (Pi->v[i]) {
+                Z->v[i] = Pi->v[i];
             } else {
-                z[i] = 0.0;
+                Z->v[i] = 0.0;
             }
         }
         printf("Vecteur fourni: \n");
-        x = z;
+
+        free(Pi->v);
+        free(Pi);
+        Pi = Z;
     }
-    //affichevec(x);
 
     float delta = 1.0;
     float epsilon = 10e-6;
     int cnt = 0;
 
-    f = malloc(C * sizeof(int));
-    w = malloc(C * sizeof(proba));
-    y = malloc(C * sizeof(proba));
-   
-    if (f == NULL || w == NULL || y == NULL) {
+    f = malloc(Mat->C * sizeof(int));
+    struct vecteur *y = alloueVecteur(Mat->C);
+    struct vecteur *w = alloueVecteur(Mat->C);
+    if (f == NULL || y == NULL || w == NULL) {
         printf("Erreur d'allocation");
         exit(40);
     }
 
-    calculeF(f, P);
-
+    calculeF(f, Mat);
     // x : PI(n)
     // y : PI(n+1)
     while (delta > epsilon) {
         cnt ++;
         metZero(y);
         metZero(w);
-        iterer(x, y, w, P);
-
-        //printf("pi %d :\n", cnt);
-        //affichevec(y);
-        delta = normeVect(x, y);
-        recopie(x, y);
+        iterer(Pi, y, w, Mat);
+        delta = normeVect(Pi, y);
+        recopie(Pi, y);
     }
 
     free(f);
     free(y);
-    return x;
+    return Pi;
 }
 
-int main(int argc, char *argv[]) {
+FILE *initialiseGNU() {
 
     FILE *gnuplot = popen("gnuplot", "w");
     if (gnuplot) {
@@ -266,43 +285,56 @@ int main(int argc, char *argv[]) {
         fprintf(gnuplot, "set grid\n");
 
         fprintf(gnuplot, "plot '-' with linespoints title 'Convergence'\n");
+
+        return gnuplot;
+    } else { return 0; }
+}
+
+int main(int argc, char *argv[]) {
+
+    FILE *gnuplot = initialiseGNU();
+    if (gnuplot) {
         
         int cnt = 0;
-        double pas = 100.;
+        double pas = 30.;
         char nom[] = "Matrices/StanfordBerkeley.txt";
-        struct elem *P = lectureMatrice(nom);
+        struct matrice *Mat = lectureMatrice(nom);
+
         for (int i = 0; i < pas - 1; i++) {
+
             alpha = 0 + (i * (1 / pas));
             invAlpha = 1 - alpha;
             long long moyenne_temps = 0;            
             long moyenne = 1.;
+
             for (int index = 0; index < moyenne; index++) {
+
                 cnt ++;
-                printf("Tour %d\n", cnt);
                 gettimeofday(&t1, NULL);
-                puissances(P, 0);
+                puissances(Mat, 0);
                 gettimeofday(&t2, NULL);
                 moyenne_temps += (t2.tv_sec - t1.tv_sec) * 1000000L + (t2.tv_usec - t1.tv_usec);
+
             }
+
             fprintf(gnuplot, "%f %f\n", alpha, (moyenne_temps / moyenne) / 1e6);
             printf("%f, %f\n", alpha, (moyenne_temps/moyenne) / 1e6);     
         }
     }
+
     fprintf(gnuplot, "e\n");
     fprintf(gnuplot, "unset output\n");
 
-//    printf("\nitération 2\n");
-//    x = puissances("matriceCreuseV2.txt", x);
-//
-//    printf("\nitération 3\n");
-//    x = puissances("matriceCreuseV3.txt", x);
+    //    printf("\nitération 2\n");
+    //    x = puissances("matriceCreuseV2.txt", x);
+    //
+    //    printf("\nitération 3\n");
+    //    x = puissances("matriceCreuseV3.txt", x);
 
     // gettimeofday(&t1, NULL);
     // puissances("Matrices/StanfordBerkeley.txt", 0);
     // gettimeofday(&t2, NULL);
     // long temps_micro = (t2.tv_sec - t1.tv_sec) * 1000000L + (t2.tv_usec - t1.tv_usec);
     // printf("Converge en %ld micro sec\n", temps_micro);
-    free(x);
-    free(P);
     return 0;
 }
